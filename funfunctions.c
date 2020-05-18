00edfe3617a4cd97654d4107e13742a9ca04a6fd
Richy Castro <rcastro2@radford.edu>
2020-05-18 11:32:06 -0400

    Exercise 5-3

--- funfunctions.c
+++ funfunctions.c
@@ -38,3 +38,40 @@ int strRIndex(char string[], char target)
 
     return targIndex;
 }
+
+/*
+ * strcat: concatenate t to end of s; s must be big enough (array version)
+ * provided by "C Programming Language" book
+
+void strcat(char s[], char t[])
+{
+    int i, j;
+
+    i = j = 0;
+        while (s[i] != '\0')     // find end of s
+            i++;
+        while ((s[i++] = t[j++]) != '\0')     // copy t
+            ;
+}
+*/
+
+/*
+ * strcat: concatenate t to end of s; s must be big enough (pointer version)
+ * @author Richy
+ * Exercise 5-3 Write a pointer version of the function strcat that we showed in chapter 2
+ * strcat(s, t) copies the string t to the end of s.
+ */
+void strcat(char *s, char *t)
+{
+    while(*s != '\0')
+        s++;
+    while((*s = *t) != '\0') {
+        s++;
+        t++;
+    }
+}
+
+int main()
+{
+
+}
